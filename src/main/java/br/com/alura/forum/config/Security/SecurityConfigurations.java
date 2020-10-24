b6f96e8f34412738b7a63f4a6ad927aeee488723
cleberGraciano <cleber_batista2010@hotmail.com>
2020-10-24 18:00:14 -0300

    Baixar configurar e habilitar Spring security

---
+++ src/main/java/br/com/alura/forum/config/Security/SecurityConfigurations.java
@@ -0,0 +1,11 @@
+package br.com.alura.forum.config.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+}
