da5542ac8f9be6240078febdcb7888cae354d56c
cleberGraciano <cleber_batista2010@hotmail.com>
2020-10-24 18:18:53 -0300

    Liberando acesso ao recurso GET/topicos com Spring Security

--- src/main/java/br/com/alura/forum/config/Security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/Security/SecurityConfigurations.java
@@ -1,6 +1,10 @@
 package br.com.alura.forum.config.Security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,4 +12,25 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
+    //Configurações de Autorização
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+
+    }
+
+    //Configurações de autenticação
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    }
+
+    //configurações de recursos estaticos (.js, .css, imagens)
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+    }
 }
